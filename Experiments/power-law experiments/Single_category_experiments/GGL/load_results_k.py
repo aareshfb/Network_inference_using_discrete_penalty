1c3a61bba5d8614498f9788b31cc8776bf6c81da
Aaresh Bhathena <78177666+aareshfb@users.noreply.github.com>
2026-04-17 17:30:51 -0400

    Add files via upload

---
+++ Experiments/power-law experiments/Single_category_experiments/GGL/load_results_k.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import os
+
+# Input lists
+seed_list = [1994, 1995, 1996, 1997, 1998]
+np_list = [3, 5, 10, 20, 50, 100 ]
+
+# Output file
+results_file = os.path.join("Output","results_k.csv")
+with open(results_file, 'w') as f:
+    f.write('rand_seed,t,p,n,nu_0,mu,gamma,bic value,precision,recall,F1,Time(min)')
+    f.write('\n')
+
+# Initialize results DataFrame
+results_df = pd.DataFrame()
+
+for seed in seed_list:
+    for np_val in np_list:
+        np_str = str(np_val).replace('.', 'd')  # e.g., 0.1 → 01
+        folder = os.path.join("Output", str(seed))
+        filename = f"GGL_CPU20_L{np_str}.csv"
+        file_path = os.path.join(folder, filename)
+
+        try:
+            df = pd.read_csv(file_path)
+        except FileNotFoundError:
+            print(f"File {file_path} not found. Skipping.")
+            continue
+
+        if " BIC" not in df.columns:
+            print(f"' BIC' column not found in {file_path}. Skipping.")
+            continue
+        
+        
+        
+        # Get row with smallest BIC
+        df[" BIC"] = pd.to_numeric(df[" BIC"], errors='coerce')
+        df = df.dropna(subset=[" BIC"])
+        
+        min_bic_idx = df[" BIC"].idxmin()
+        best_row = df.loc[min_bic_idx]
+
+        # Tag row with seed and np_val
+        best_row["seed"] = seed
+        best_row["n_by_p"] = np_val
+
+        results_df = results_df.append(best_row, ignore_index=True)
+
+# Save combined results
+results_df.to_csv(results_file, index=False)
+print(f"Saved best rows to {results_file}")
